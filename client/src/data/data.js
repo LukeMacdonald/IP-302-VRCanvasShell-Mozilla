d786f500930a4b457fa5c1b3fe48979e90461491
LukeMacdonald <lukemacdonald21@gmail.com>
2023-09-08 19:32:01 +1000

    added exception handling to api calls

--- client/src/data/data.js
+++ client/src/data/data.js
@@ -7,256 +7,359 @@ const COURSE_KEY = "course";
 const objectPositions = ["0 2 0", "0 2 -2", "0 2 -4", "0 2 -8"]
 
 
-async function setCourse(courseID){
-
-    const response = await fetch(`http://localhost:3001/data/${courseID}`, {
+async function setCourse(courseID) {
+    try {
+      const response = await fetch(`http://localhost:3001/data/${courseID}`, {
         method: 'GET',
         headers: {
-            'Content-Type': 'application/json',
+          'Content-Type': 'application/json',
         },
-    });
-    const data = await response.json()
-    localStorage.setItem(DATA_KEY,  JSON.stringify(data))
-}
+      });
+      if (!response.ok) {
 
-function setLocalData(data){
-    localStorage.setItem(DATA_KEY,JSON.stringify(data))
-}
+        throw new Error(`Failed to fetch data for course ${courseID}`);
+      }
+      const data = await response.json();
+      localStorage.setItem(DATA_KEY, JSON.stringify(data));
+    } catch (error) {
+      console.error('Error:', error);
+      // Handle the error as needed (e.g., show a user-friendly error message)
+      throw error;
+    }
+  }
 
-function getData(){
-    return JSON.parse(localStorage.getItem(DATA_KEY));
-}
+  function setLocalData(data) {
+    try {
+      localStorage.setItem(DATA_KEY, JSON.stringify(data));
+    } catch (error) {
+      console.error('Error setting local data:', error);
+      // Handle the error as needed (e.g., show a user-friendly error message)
+      throw error;
+    }
+  }
 
-function getModules(courseID){
+  function getData() {
+    try {
+      const storedData = localStorage.getItem(DATA_KEY);
+      return storedData ? JSON.parse(storedData) : null;
+    } catch (error) {
+      console.error('Error getting data from local storage:', error);
+      // Handle the error as needed (e.g., show a user-friendly error message)
+      throw error;
+    }
+  }
+
+function getModules(courseID) {
     setCourse(courseID);
     const data = getData();
-    const modules = data["modules"];
-    const isEmpty = Object.keys(modules).length === 0; 
-    if (isEmpty){
-        return {};
-    }
-    else{
-        return data["modules"];
-    }
-}
+    return data.modules || {};
+  }
+  
+  function getModule(moduleName) {
+    const data = getData();
+    return data.modules ? data.modules[moduleName] || {} : {};
+  }
 
-function getModule(moduleName){
-    const data = getData()
-    const module = data["modules"][moduleName]
-    const isEmpty = Object.keys(module).length === 0;
-    if (isEmpty){
-        return {};
-    }
-    else{
-        return module;
-    }
-    
-}
-
-function getRoom(moduleName,roomName){
-    const data = getData()
-    return data["modules"][moduleName]["rooms"][roomName]
-}
-
-function getCourseID(){
-    return JSON.parse(localStorage.getItem(COURSE_KEY));
-}
-
-async function getCourses(){
-    const endpoint = "http://localhost:3001/course/teacher";
-    try{
-        const response = await fetch(endpoint);
-        const courses = await response.json();
-        return courses;
+function getRoom(moduleName, roomName) {
+    try {
+      const data = getData();
+      if (data && data.modules && data.modules[moduleName] && data.modules[moduleName].rooms) {
+        const roomData = data.modules[moduleName].rooms[roomName];
+        if (roomData !== undefined) {
+          return roomData;
+        }
+      }
+      // Handle the missing data structure or roomName not found
+      throw new Error("Room not found");
     } catch (error) {
-        console.error("Error:", error);
-        throw error;
+      console.error("Error:", error);
+      // Return a default value or handle the error appropriately
+      throw error;
     }
-}
+  }
 
-function getCoursefiles(){
-    const files = localStorage.getItem(FILES_KEY);
-    return JSON.parse(files)
-}
-async function setCourseFiles(courseID){
+  function getCourseID() {
+    try {
+      const courseID = JSON.parse(localStorage.getItem(COURSE_KEY));
+      if (courseID !== null) {
+        return courseID;
+      }
+      // Handle the case when courseID is null or undefined
+      throw new Error("Course ID not found");
+    } catch (error) {
+      console.error("Error:", error);
+      // Return a default value or handle the error appropriately
+      throw error;
+    }
+  }
 
-    await setCourse(courseID)
-    
-    localStorage.setItem(COURSE_KEY,JSON.stringify(courseID))
-    
-    const response = await fetch(`http://localhost:3001/files/${courseID}`, {
+async function getCourses() {
+    try {
+      const endpoint = "http://localhost:3001/course/teacher";
+      const response = await fetch(endpoint);
+  
+      if (!response.ok) {
+        throw new Error(`HTTP Error: ${response.status}`);
+      }
+  
+      const courses = await response.json();
+      return courses;
+    } catch (error) {
+      console.error("Error:", error);
+      throw error;
+    }
+  }
+
+function getCoursefiles() {
+    try {
+      const files = localStorage.getItem(FILES_KEY);
+      return JSON.parse(files) || null;
+    } catch (error) {
+      console.error("Error:", error);
+      throw error;
+    }
+  }
+async function setCourseFiles(courseID) {
+    try {
+      // Set the course
+      await setCourse(courseID);
+      
+  
+      // Store courseID in localStorage
+      localStorage.setItem(COURSE_KEY, JSON.stringify(courseID));
+  
+      // Fetch course files
+      const response = await fetch(`http://localhost:3001/files/${courseID}`, {
         method: 'GET',
         headers: {
-            'Content-Type': 'application/json',
+          'Content-Type': 'application/json',
         },
-    });
-
-    const data = await response.json()
-    localStorage.setItem(FILES_KEY,  JSON.stringify(data))
-}
+      });
+  
+      if (!response.ok) {
+        throw new Error(`HTTP Error: ${response.status}`);
+      }
+  
+      const data = await response.json();
+  
+      // Store fetched data in localStorage
+      localStorage.setItem(FILES_KEY, JSON.stringify(data));
+    } catch (error) {
+      console.error("Error:", error);
+      throw error;
+    }
+  }
 
 async function addRoomtoModuleFunction(roomData) {
-    const endpoint = "http://localhost:3001/room/create";
-
-    const requestOptions = {
+    try {
+      const endpoint = "http://localhost:3001/room/create";
+  
+      const requestOptions = {
         method: "POST",
         headers: {
-            "Content-Type": "application/json",
+          "Content-Type": "application/json",
         },
         body: JSON.stringify(roomData),
-    };
-
-    try {
-        const response = await fetch(endpoint, requestOptions);
-        const data = await response.json();
-        return data;
+      };
+  
+      const response = await fetch(endpoint, requestOptions);
+  
+      if (!response.ok) {
+        throw new Error(`HTTP Error: ${response.status}`);
+      }
+  
+      const data = await response.json();
+      return data;
     } catch (error) {
-        console.error("Error:", error);
-        throw error;
+      console.error("Error:", error);
+      throw error;
     }
-}
+  }
 
 async function addRoomtoModule(module, roomName, roomObjects) {
     try {
-        const objects = [];
-        roomObjects.forEach((object, index) => {
-            if (object.url !== "") {
-
-                const data = {
-                    "name": object.display_name,
-                    "url": object.url,
-                    "position": objectPositions[index]
-                };
-                objects.push(data);
-            }
-        });
-        const roomData = {
-            "roomName": roomName,
-            "objects": objects
-        };
-
-        const response = await addRoomtoModuleFunction(roomData);
-
-        const storedData = {
-            "RoomID": response.id,
-            "Objects": objects
-        }
-
-        const data = getData()
-        data["modules"][module]["rooms"][roomName] = storedData
-        saveData(data)
-        setLocalData(data)
-        
+      const objects = roomObjects
+        .filter((object) => object.url !== "")
+        .map((object, index) => ({
+          name: object.display_name,
+          url: object.url,
+          position: objectPositions[index],
+        }));
+  
+      const roomData = {
+        roomName,
+        objects,
+      };
+  
+      const response = await addRoomtoModuleFunction(roomData);
+  
+      const storedData = {
+        RoomID: response.id,
+        Objects: objects,
+      };
+  
+      const data = getData();
+      data.modules[module].rooms[roomName] = storedData;
+  
+      // Save and set local data
+      saveData(data);
+      setLocalData(data);
     } catch (error) {
-        // Handle the error
+      console.error("Error:", error);
+      throw error;
+      // Handle the error
     }
-}
+  }
 
 async function saveData(data) {
-    const endpoint = "http://localhost:3001/course/save";
-    const courseID = getCourseID();
-    const request = { "data": data, "courseID": courseID };
-
-    const requestOptions = {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(request),
-    };
-
     try {
-        const response = await fetch(endpoint, requestOptions);
-        const responseData = await response.json();
-        return responseData;
-    } catch (error) {
-        console.error("Error:", error);
-        throw error;
-    }
-}
-
-async function loadRoom(module,roomID){
-
-    const endpoint = "http://localhost:3001/reload-room"
-
-    const courseID = getCourseID();
-
-    const request = { "moduleName": module, "courseID": courseID, "roomID":roomID };
-    
-    const requestOptions = {
+      const endpoint = "http://localhost:3001/course/save";
+      const courseID = getCourseID();
+      const request = {
+        data,
+        courseID,
+      };
+  
+      const requestOptions = {
         method: "POST",
-        headers: { "Content-Type": "application/json" },
+        headers: {
+          "Content-Type": "application/json",
+        },
         body: JSON.stringify(request),
-    };
-
+      };
+  
+      const response = await fetch(endpoint, requestOptions);
+  
+      if (!response.ok) {
+        throw new Error(`HTTP Error: ${response.status}`);
+      }
+  
+      const responseData = await response.json();
+      return responseData;
+    } catch (error) {
+      console.error("Error:", error);
+      throw error;
+    }
+  }
+  
+async function loadRoom(module, roomID) {
     try {
-        const response = await fetch(endpoint, requestOptions);
-        const responseData = await response.json();
-        window.open(responseData.url, '_blank');
-        return responseData;
-    } catch (error) {
-        console.error("Error:", error);
-        throw error;
-    }
-}
-
-async function getCanvasCourseModules(modules){
-    const courseID = getCourseID();
-    const endpoint = `http://localhost:3001/modules/${courseID}`;
-    try{
-        const response = await fetch(endpoint);
-        const canvasModules = await response.json();
-        const availableModules = []
-        canvasModules.forEach((module) => {
-            console.log(module);
-            console.log(Object.keys(modules));
-            if (!Object.keys(modules).includes(module.id.toString())) {
-                // module.name is not in the modules object
-                availableModules.push(module);
-            }
-        });
-        return availableModules;
-    } catch (error) {
-        console.error("Error:", error);
-        throw error;
-    }
-}
-
-async function getCanvasCourseModuleFiles(moduleID){
-
-    const courseID = getCourseID();
-    const endpoint = `http://localhost:3001/modules/files/${courseID}/${moduleID}`;
-    try{
-        const response = await fetch(endpoint);
-        const canvasFiles = await response.json();
-        return canvasFiles;
-    } catch (error) {
-        console.error("Error:", error);
-        throw error;
-    }
-}
-
-async function createCourseModule(moduleID, moduleName){
-    const courseID = getCourseID();
-    
-    const request = { "moduleID":moduleID,"moduleName":moduleName, "courseID": courseID };
-    
-    const endpoint = "http://localhost:3001/module/create";
-    
-    const requestOptions = {
+      const courseID = getCourseID();
+      const endpoint = "http://localhost:3001/reload-room";
+  
+      const request = {
+        moduleName: module,
+        courseID: courseID,
+        roomID: roomID,
+      };
+  
+      const requestOptions = {
         method: "POST",
-        headers: { "Content-Type": "application/json" },
+        headers: {
+          "Content-Type": "application/json",
+        },
         body: JSON.stringify(request),
-    };
-
-    try {
-        const response = await fetch(endpoint, requestOptions);
-        const responseData = await response.json();
-        localStorage.setItem(FILES_KEY,  JSON.stringify(responseData));
-        return responseData;
+      };
+  
+      const response = await fetch(endpoint, requestOptions);
+  
+      if (!response.ok) {
+        throw new Error(`HTTP Error: ${response.status}`);
+      }
+  
+      const responseData = await response.json();
+  
+      // Open the URL in a new tab/window
+      window.open(responseData.url, "_blank");
+  
+      return responseData;
     } catch (error) {
-        console.error("Error:", error);
-        throw error;
+      console.error("Error:", error);
+      throw error;
     }
-}
+  }
+
+async function getCanvasCourseModules(modules) {
+    try {
+      const courseID = getCourseID();
+      const endpoint = `http://localhost:3001/modules/${courseID}`;
+      
+      const response = await fetch(endpoint);
+  
+      if (!response.ok) {
+        throw new Error(`HTTP Error: ${response.status}`);
+      }
+  
+      const canvasModules = await response.json();
+  
+      // Use filter and Set for efficient module filtering
+      const moduleIds = new Set(Object.keys(modules).map(Number));
+      const availableModules = canvasModules.filter((module) => !moduleIds.has(module.id));
+  
+      return availableModules;
+    } catch (error) {
+      console.error("Error:", error);
+      throw error;
+    }
+  }
+
+async function getCanvasCourseModuleFiles(moduleID) {
+    try {
+      const courseID = getCourseID();
+      const endpoint = `http://localhost:3001/modules/files/${courseID}/${moduleID}`;
+  
+      const response = await fetch(endpoint);
+  
+      if (!response.ok) {
+        throw new Error(`HTTP Error: ${response.status}`);
+      }
+  
+      const canvasFiles = await response.json();
+      return canvasFiles;
+    } catch (error) {
+      console.error("Error:", error);
+      throw error;
+    }
+  }
+
+async function createCourseModule(moduleID, moduleName) {
+    try {
+      const courseID = getCourseID();
+      const request = {
+        moduleID,
+        moduleName,
+        courseID,
+      };
+  
+      const endpoint = "http://localhost:3001/module/create";
+  
+      const requestOptions = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(request),
+      };
+  
+      const response = await fetch(endpoint, requestOptions);
+      
+      if (!response.ok) {
+        throw new Error(`HTTP Error: ${response.status}`);
+      }
+  
+      const responseData = await response.json();
+      
+      // You may want to handle the following actions outside this function
+      await setCourse(courseID);
+      localStorage.setItem(FILES_KEY, JSON.stringify(responseData));
+  
+      return responseData;
+    } catch (error) {
+      console.error("Error:", error);
+      throw error;
+    }
+  }
+  
 
 export {
     getCoursefiles,
