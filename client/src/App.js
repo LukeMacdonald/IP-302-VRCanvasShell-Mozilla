8792ffcea1e342c90ba6ee0b503392692e8041cc
LukeMacdonald <lukemacdonald21@gmail.com>
2023-09-21 01:03:08 +1000

    added auto rerouting to login if token not in localstorage

--- client/src/App.js
+++ client/src/App.js
@@ -5,25 +5,39 @@ import NotFound from './pages/NotFound';
 import Courses from './pages/Courses';
 import CreateModule from './pages/CreateModule';
 import Login from './pages/Login';
-import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import Signup from './pages/Signup';
+import { HashRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 
 function App() {
+  const token = localStorage.getItem("token");
+
+  const routeConfig = [
+    { path: "/", element: <Login /> },
+    { path: "/signin", element: <Signup /> },
+    { path: "/courses", element: token ? <Courses /> : <Navigate to="/" /> },
+    { path: "/courses/:courseID", element: token ? <Home /> : <Navigate to="/" /> },
+    { path: "/courses/:courseID/modules/add", element: token ? <CreateModule /> : <Navigate to="/" /> },
+    { path: "/courses/:courseID/:moduleID/rooms/add", element: token ? <CreateRoom /> : <Navigate to="/" /> },
+    { path: "/error", element: <NotFound /> },
+    { path: "*", element: <Navigate to="/" /> }, // Fallback route
+  ];
+
   return (
-    <div className="App"> 
+    <div className="App">
       <Router>
         <Routes>
-          <Route path="/" element={<Login />} />
-          <Route path="/signin" element={<Signup />} />
-          <Route path="/courses" element={<Courses/>}/> 
-          <Route path="/courses/:courseID" element={<Home />} />
-          <Route path="/courses/:courseID/modules/add" element={<CreateModule/>} />
-          <Route path="courses/:courseID/:moduleID/rooms/add" element={<CreateRoom/>}/>
-          <Route path="/error" element={<NotFound/>}/>
+          {routeConfig.map((route, index) => (
+            <Route
+              key={index}
+              path={route.path}
+              element={route.element}
+            />
+          ))}
         </Routes>
       </Router>
     </div>
   );
 }
+
 export default App;
 
