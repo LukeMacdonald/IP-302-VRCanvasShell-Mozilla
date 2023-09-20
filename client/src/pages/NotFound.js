3e289898675ba77d97217be61127b42a5e270788
LukeMacdonald <lukemacdonald21@gmail.com>
2023-09-21 02:59:00 +1000

    fixed redirect from error page

--- client/src/pages/NotFound.js
+++ client/src/pages/NotFound.js
@@ -9,7 +9,7 @@ const NotFound = () => {
       <div className="text-center">
         <h1 className="display-1 fw-bold">Error</h1>
         <p className="fs-3"> <span class="text-danger">Opps!</span> Something has gone wrong.</p>
-        <p className="lead">Your request could not be processed.</p><a href="/#/courses" class="btn btn-primary">Go Home</a>
+        <p className="lead">Your request could not be processed.</p><a href="/#/" class="btn btn-primary">Go Home</a>
       </div>
     </div>
     </>
