90eda5df496f2a014a686aa0708a5d387bfe203f
LukeMacdonald <lukemacdonald21@gmail.com>
2024-01-30 16:19:18 +1100

    updated config for new community edition

--- server/config/config.js
+++ server/config/config.js
@@ -1,12 +1,21 @@
-require('dotenv').config();
-const fs = require('fs')
+require("dotenv").config();
+const fs = require("fs");
 
-exports.HUBS_PUBLIC_URL= "https://canvas-hub.com/"
-exports.CANVAS_BASE_URL= "https://rmit.instructure.com/api/v1/"
+exports.HUBS_PUBLIC_URL = "https://test.canvas-hub.com/";
+exports.CANVAS_BASE_URL = "https://rmit.instructure.com/api/v1/";
 
-exports.allowedExtensions = ['.jpg', '.jpeg', '.png', '.gif','.glb', '.gltf', '.pdf', '.mp4'];
+exports.allowedExtensions = [
+  ".jpg",
+  ".jpeg",
+  ".png",
+  ".gif",
+  ".glb",
+  ".gltf",
+  ".pdf",
+  ".mp4",
+];
 
-exports.enrollementTypes={
-    Student: "student",
-    Staff: "teacher"
-}
+exports.enrollementTypes = {
+  Student: "student",
+  Staff: "teacher",
+};
--- server/index.js
+++ server/index.js
@@ -1,20 +1,20 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const db = require("./database")
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const db = require("./database");
 
 app.use(cors());
 
 app.use(bodyParser.json());
 
-require('dotenv').config();
+require("dotenv").config();
 
 // Add routes.
-require('./routes/canvas-routes')(express, app);
-require('./routes/hubs-routes')(express, app);
-require('./routes/database-routes')(express, app);
-
+require("./routes/canvas-routes")(express, app);
+require("./routes/hubs-routes")(express, app);
+require("./routes/database-routes")(express, app);
+require("./routes/quiz-routes")(express, app);
 // Configuration
 const PORT = process.env.PORT || 3000;
 
@@ -23,3 +23,4 @@ app.listen(PORT, () => {
   console.log(`HTTP Application listening on port ${PORT}!`);
   console.log(`Go to http://localhost:${PORT}/`);
 });
+
