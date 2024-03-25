39c91e44a222b3358c7c6fa47fb808932bf4b747
LukeMacdonald <82664752+LukeMacdonald@users.noreply.github.com>
2024-03-25 14:51:11 +1100

    updated courses card

--- client/src/components/Courses.js
+++ client/src/components/Courses.js
@@ -4,26 +4,23 @@ import { useDispatch } from "react-redux";
 import { setMyCourses, setCourse } from "../redux/reducers";
 import { getCourses } from "../database/api";
 
-
 function CourseCard(props) {
   const course = props.course;
   const dispatch = useDispatch();
 
-  
   return (
-    <a 
-      className="m-2 p-2 w-1/2 md:w-full bg-gray-700 text-light rounded-md  hover:bg-gray-900 tracking-wide truncate border-l-4 border-transparent hover:border-red-500" 
-      href={`/#/courses/${course.id}`} 
+    <a
+      className="m-2 p-2 w-1/2 md:w-full bg-gray-700 text-light rounded-md  hover:bg-gray-900 tracking-wide truncate border-l-4 border-transparent hover:border-red-500"
+      href={`/#/courses/${course.id}`}
       onClick={() => dispatch(setCourse(props.course))}
     >
       <h1 className="text-3xl py-3">{course.name}</h1>
-      <hr/>
+      <hr />
       <p className="py-2">{course.course_code}</p>
       <p className="py-2">{course.time_zone}</p>
     </a>
   );
 }
-
 
 function Courses() {
   const [courses, setCourses] = useState([]);
@@ -48,18 +45,16 @@ function Courses() {
     fetchData();
   }, [fetchCourses]);
   return (
-    <div className="w-full flex flex-col justify-start items-start px-5 py-4" >
+    <div className="w-full flex flex-col justify-start items-start px-5 py-4">
       <h1 className="self-start text-lg">My Courses</h1>
-      <hr className="w-full"/>
+      <hr className="w-full" />
       <div className="w-full flex flex-wrap justify-start items-center">
-      {courses.map((course, index) => (
-        <CourseCard course={course} />
-         
+        {courses.map((course, index) => (
+          <CourseCard key={index} course={course} />
         ))}
-
       </div>
-
     </div>
   );
 }
 export default Courses;
+
